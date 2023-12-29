3b778350d35e4b668490ebea4306085a6be22a2c
JishnuVijayan <108149185+JishnuVijayan@users.noreply.github.com>
2023-12-29 23:09:16 +0530

    Font style changed.
    Font Style is changed in Login page.

--- src/Components/Login_Component.js
+++ src/Components/Login_Component.js
@@ -4,27 +4,23 @@ import "../Styles/Login_Component.css";
 export default function Login_Component() {
   return (
     <div className="Main">
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
-      ></link>
       <div className="Box1">
-        <h1 className="Font"> LOGIN</h1>
+        <h1 className="Font"> Login</h1>
         <input
           type="email"
           autocomplete="off"
           name="email"
-          class="input"
+          className="input"
           placeholder="Email"
         ></input>
         <input
           type="password"
           autocomplete="off"
           name="password"
-          class="input"
+          className="input"
           placeholder="Password"
         ></input>
-        <button>LOGIN</button>
+        <button>Login</button>
       </div>
       <div className="Divider"></div>
       <div className="Box2">
@@ -37,8 +33,8 @@ export default function Login_Component() {
               paddingTop: "35%",
             }}
           >
-            <h2 className="Font">NEW HERE ?</h2>
-            <button>SIGNUP</button>
+            <h2 className="Font">New here?</h2>
+            <button>Signup</button>
           </div>
         </div>
       </div>
